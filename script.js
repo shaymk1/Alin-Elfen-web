04a85cf44fcf7a91748638cf28682703763fc8fe
Mamosebetsi Kekae <mkekae@gmail.com>
2022-06-28 13:37:57 -0700

    cleaned up the files

--- script.js
+++ script.js
@@ -16,5 +16,5 @@ menuBtn.onclick = function () {
 // All animations will take exactly 500ms
 var scroll = new SmoothScroll('a[href*="#"]', {
 	speed: 1000,
-	speedAsDuration: true
+	speedAsDuration: true,
 });
